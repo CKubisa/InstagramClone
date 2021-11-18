f207359dae00408434cbefe3b5e49e3bae1a9b8b
CKubisa <ckubisa38@gmail.com>
2021-11-17 22:23:52 -0500

    fixed mongo.js

--- server/models/mongo.js
+++ server/models/mongo.js
@@ -1,9 +1,13 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ifcmm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// @ts-ignore
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ifcmm.mongodb.net/myFirstDatabase?`
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+
+const isConnected = client.connect();
+
+
+module.exports = {
+    client, isConnected
+}
